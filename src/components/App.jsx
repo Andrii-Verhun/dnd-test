4ecc021e4727aacd2ede83ee4ff43bde2706dd6b
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-08-06 20:04:14 +0300

    #1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,40 @@
+// import { Droppable } from "react-beautiful-dnd";
+import { Draggable } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd";
+import React from 'react';
+
+import { Card } from "./Card";
+import { StrictModeDroppable } from './droppableStrick';
+
+const cards = [
+  {
+    id: '0123',
+    name: 'Andrii',
+    position: 0,
+  },
+  {
+    id: '131231',
+    name: 'Oleh',
+    position: 1,
+  },
+  {
+    id: '2123123',
+    name: 'Anatolii',
+    position: 2,
+  },
+  {
+    id: '3123213',
+    name: 'Max',
+    position: 3,
+  },
+]
+
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        width: '100vw',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -10,7 +42,63 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <DragDropContext >
+        <StrictModeDroppable droppableId="droppable-1">
+          {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {cards.map((el) => {
+                  return (
+                    <Draggable key={el.id} draggableId={el.id} index={el.position}>
+                      {(provided, snapshot) => (
+                          <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className="card"
+                          >
+                            <Card draggableId={el.id}  name={el.name} />
+                          </div>
+                      )}
+                    </Draggable>
+                    
+                  )})
+                }
+                {provided.placeholder}
+              </div>
+            )}
+        </StrictModeDroppable>
+          {/* <Droppable droppableId="droppable-1">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {cards.map((el) => {
+                  return (
+                    <Draggable key={el.id} draggableId={el.id} index={el.position}>
+                      {(provided, snapshot) => (
+                          <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className="card"
+                          >
+                            <Card draggableId={el.id}  name={el.name} />
+                          </div>
+                      )}
+                    </Draggable>
+                    
+                  )})
+                }
+                {provided.placeholder}
+              </div>
+            )}
+        </Droppable> */}
+      </DragDropContext>
     </div>
+
   );
 };
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+// import { Draggable } from "react-beautiful-dnd";
+
+export const Card = ({name, id, pos}) => {
+    return (
+        <h4>{name}</h4>
+    )
+}
